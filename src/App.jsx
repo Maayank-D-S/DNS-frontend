2578761e181a103978ea068991ac60797ce92d8c
Mayank-209 <msknowsevt@gmail.com>
2025-07-21 01:04:46 +0530

    chnges

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import StoryPage from "./pages/StoryPage"; // Make sure this file exists
 import ImpactPage from "./pages/ImpactPage";
-import ProjectPage from "./pages/ProjectPage";
-import About from "./pages/Project/About";
-import Gallery from "./pages/Project/Gallery";
-import Location from "./pages/Project/Location";
-import Price from "./pages/Project/Price";
+
+
+
 import BlogGrid from "./pages/BlogPage";
 import BlogContent from "./pages/BlogContent";
 
@@ -19,13 +17,7 @@ function App() {
         <Route path="/impact" element={<ImpactPage />} />
         <Route path="/blogs" element={<BlogGrid/>} />
         <Route path="/blog/:id" element={<BlogContent/>} />
-        <Route path="/project" element={<ProjectPage />}>
-          <Route path="about" element={<About />} />
-          {/* <Route path="amenities" element={<Amenities />} /> */}
-          <Route path="location" element={<Location />} />
-          <Route path="prices" element={<Price />} />
-          <Route path="gallery" element={<Gallery />} />
-        </Route>
+        
       </Routes>
     </Router>
   );
